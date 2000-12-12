e8c9577b6469ce5521e82f48b55ef0db3dc889b3
Martin Maechler <maechler@r-project.org>
2000-12-12 23:42:25 +0000

    version of 2000.12.12.23.42.25 (now is 16 May 2011).

--- R/dip.R
+++ R/dip.R
@@ -7,29 +7,20 @@
 
 dip <- function(x, full.result = FALSE, debug = FALSE)
 {
-  ## Purpose: Compute the "Dip test for unimodality" (statistic)
-  ## -------------------------------------------------------------------------
-  ## Arguments: x: the Data, full.result: return also 'xl', 'xu' (modal interv.)
-  ## -------------------------------------------------------------------------
-  ## Author: Martin Maechler <maechler@stat.math.ethz.ch>, Jul 94
-  ##      from 1st version of   Dario Ringach <dario@wotan.cns.nyu.edu>
-
   n <- length(x)
-
-  ii <- if(full.result) 1:11 else  "dip"
-  simplify <- function(lis) if(length(lis)==1) as.single(lis[[1]]) else lis
-  simplify(.C("diptst",
-	      x   = as.single(sort(x)),
-	      n   = as.integer(n),
-	      dip = single(1),
-	      xl  = single(1),
-	      xu  = single(1),
-	      ifault= integer(1),
-	      gcm =   integer(n),
-	      lcm =   integer(n),
-	      mn  =   integer(n),
-	      mj  =   integer(n),
-	     debug= as.integer(debug))[ii])
-  ##-   if(z$ifault!=0)  #-- something not ok, but this is IMPOSSIBLE here
-  ##-     stop(paste("Problem -- C 'message' : ifault = ", z$ifault))
+  (function(lis) if(length(lis)==1) lis[[1]] else lis
+   ) (.C("diptst",
+         x   = as.double(sort(x)),
+         n   = as.integer(n),
+         dip = double(1),
+         xl  = double(1),
+         xu  = double(1),
+         ifault= integer(1),
+         gcm =   integer(n),
+         lcm =   integer(n),
+         mn  =   integer(n),
+         mj  =   integer(n),
+         debug= as.logical(debug))[if(full.result) TRUE else "dip"])
+##-   if(z$ifault)  #-- something not ok, but this is IMPOSSIBLE here
+##-     stop(paste("Problem -- C 'message' : ifault = ", z$ifault))
 }
