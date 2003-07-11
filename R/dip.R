b1d15e6373d973389550185dc7ac2673c8b8967b
Martin Maechler <maechler@r-project.org>
2003-07-11 17:00:08 +0000

    version of 2003.07.11.17.00.08 (now is 16 May 2011).

--- R/dip.R
+++ R/dip.R
@@ -8,19 +8,20 @@
 dip <- function(x, full.result = FALSE, debug = FALSE)
 {
   n <- length(x)
-  (function(lis) if(length(lis)==1) lis[[1]] else lis
-   ) (.C("diptst",
-         x   = as.double(sort(x)),
-         n   = as.integer(n),
-         dip = double(1),
-         xl  = double(1),
-         xu  = double(1),
-         ifault= integer(1),
-         gcm =   integer(n),
-         lcm =   integer(n),
-         mn  =   integer(n),
-         mj  =   integer(n),
-         debug= as.logical(debug))[if(full.result) TRUE else "dip"])
-##-   if(z$ifault)  #-- something not ok, but this is IMPOSSIBLE here
-##-     stop(paste("Problem -- C 'message' : ifault = ", z$ifault))
+  r <- .C("diptst",
+          x   = as.double(sort(x)),
+          n   = n,
+          dip = double(1),
+          xl  = double(1),
+          xu  = double(1),
+          ifault= integer(1),
+          gcm =   integer(n),
+          lcm =   integer(n),
+          mn  =   integer(n),
+          mj  =   integer(n),
+          debug= as.logical(debug),
+          PACKAGE = "diptest")[if(full.result) TRUE else "dip"]
+  ##-   if(z$ifault)  #-- something not ok, but this is IMPOSSIBLE here
+  ##-     stop(paste("Problem -- C 'message' : ifault = ", z$ifault))
+  if(full.result) r[[1]] else r
 }
