9a68c89d11d70225cc4c8a734cfa3aba65af829a
Martin Maechler <maechler@r-project.org>
2003-07-12 22:25:52 +0000

    version of 2003.07.12.22.25.52 (now is 30 Nov 2020).

--- stuff/new-simul.R
+++ stuff/new-simul.R
@@ -5,18 +5,19 @@
 library(diptest)
 
 nn <- c(4:10, 15, 20, 30, 50, 100, 200, 500, 1000, 2000, 5000)
-Ns <- 100001
-##    ~~~~~~ number of "simulations" (i.e. samples for each n in nn)
+Ns <- 1000001
+##    ~~~~~~~ number of "simulations" (i.e. samples for each n in nn)
 
 ## which percentages
 p.hi <- sort(1 - c(outer(c(1,2,5), 2:5, function(x,y) x*10^-y)))
 p.lo <- c(1,2,5)/100
-(P.p <- c(p.lo, (1:9)/10, p.hi))
+(P.p <- c(0, p.lo, (1:9)/10, p.hi, 1))# 0 & 1: the extremes are "interesting" here
 
 ## Proof that "quantile()" is just order statistics for this `Ns' :
 iq.p <- 1 + P.p * (Ns - 1)
-U <- runif(Ns)
-stopifnot(identical(sort(U)[iq.p],  quantile(U, P.p, names=FALSE)))
+##- U <- runif(Ns)
+##- stopifnot(identical(sort(U)[iq.p], ## , partial=iq.p, method="quick"
+##-                     quantile(U, P.p, names=FALSE)))
 
 P.dip <- matrix(nrow= length(nn), ncol = length(P.p),
 		dimnames = list(paste(nn), paste(formatC(P.p,w=1))))
@@ -37,7 +38,7 @@ for(n in nn) {
     Tcpu <- Tcpu + cpu
 }
 
-save(nn, Ns, P.p, P.dip, file="dipSim_1e5.rda", compress=TRUE)
+save(nn, Ns, P.p, P.dip, file="dipSim_1e6.rda", compress=TRUE)
 
 cat("\nTotal CPU = ", paste(formatC(Tcpu), collapse=", "),"\n\n")
 
