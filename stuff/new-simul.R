71364aaa6a873cc096b0aded2f515077f00b8be1
Martin Maechler <maechler@r-project.org>
2003-11-13 09:23:37 +0000

    version of 2003.11.13.09.23.37 (now is 30 Nov 2020).

--- stuff/new-simul.R
+++ stuff/new-simul.R
@@ -3,6 +3,8 @@
 ##-> ./dip-simul.S
 ##-> ./sim1.R
 library(diptest)
+pd <- package.description("diptest")
+cat("diptest package version ", pd['Version']," (",pd['Date'],")\n",sep="")
 
 nn <- c(4:10, 15, 20, 30, 50, 100, 200, 500, 1000, 2000, 5000)
 Ns <- 1000001
@@ -13,11 +15,11 @@ p.hi <- sort(1 - c(outer(c(1,2,5), 2:5, function(x,y) x*10^-y)))
 p.lo <- c(1,2,5)/100
 (P.p <- c(0, p.lo, (1:9)/10, p.hi, 1))# 0 & 1: the extremes are "interesting" here
 
-## Proof that "quantile()" is just order statistics for this `Ns' :
-iq.p <- 1 + P.p * (Ns - 1)
-##- U <- runif(Ns)
-##- stopifnot(identical(sort(U)[iq.p], ## , partial=iq.p, method="quick"
-##-                     quantile(U, P.p, names=FALSE)))
+## "quantile()" is just order statistics X[k] (k := iq.p)  for this `Ns' :
+(iq.p <- 1 + P.p * (Ns - 1))
+U <- runif(Ns)
+stopifnot(identical(sort(U)[iq.p], ## , partial=iq.p, method="quick"
+                    quantile(U, P.p, names=FALSE)))
 
 P.dip <- matrix(nrow= length(nn), ncol = length(P.p),
 		dimnames = list(as.character(nn), formatC(P.p,w=1,digits=7)))
@@ -33,7 +35,7 @@ for(n in nn) {
                        dip.n[i] <- dip(runif(n))
                    }
                        )[1:3]
-    P.dip[paste(n),] <- quantile(dip.n, p = P.p, names=FALSE)
+    P.dip [paste(n),] <- quantile(dip.n, p = P.p, names=FALSE)
     cat("\nn=",n,", cpu=", paste(formatC(cpu), collapse=", "),"\n\n")
     Tcpu <- Tcpu + cpu
 }
