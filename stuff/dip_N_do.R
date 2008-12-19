37480c308040dc1fdea94b7cf700d794e49ccaa4
Martin Maechler <maechler@r-project.org>
2008-12-19 17:29:42 +0000

    version of 2008.12.19.17.29.42 (now is 30 Nov 2020).

--- stuff/dip_N_do.R
+++ stuff/dip_N_do.R
@@ -1,13 +1,19 @@
+### This is sourced from a file that sets  N , n.sim  , .Random.seed  ...
+###
 stopifnot(exists("N"),     is.numeric(N),     length(N) == 1, N == round(N),
           exists("n.sim"), is.numeric(n.sim), length(n.sim) == 1,
           n.sim == round(n.sim))
+if(exists(".Random.seed")) {
+    cat("using \n")
+    dump(".Random.seed", "")
+} else
+    set.seed(12)
 stopifnot(require("diptest"))
 setwd("/u/maechler/R/Pkgs/diptest/stuff")
 
 dd <- numeric(n.sim)
 .pt <- proc.time()
 
-set.seed(12)
 for(i in 1:n.sim) {
     dd[i] <- dip(runif(N))
     if(i %% 100 == 0) {
@@ -23,3 +29,5 @@ nam <- paste("dip", floor(N/1000),"k", sep='')
 assign(nam, dd)
 (outf <- paste(nam, "rda", sep="."))
 save(list = c(nam, "N"), file = outf)
+
+sessionInfo()
