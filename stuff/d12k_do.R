9d4f45d0845d4f72ddb10ce9ab6289986d16b637
Martin Maechler <maechler@r-project.org>
2008-12-19 16:16:39 +0000

    Initial revision

---
+++ stuff/d12k_do.R
@@ -0,0 +1,24 @@
+stopifnot(require("diptest"))
+setwd("/u/maechler/R/Pkgs/diptest/stuff")
+
+N <- 8000
+n.sim <- 1000001
+dd <- numeric(n.sim)
+.pt <- proc.time()
+
+set.seed(12)
+for(i in 1:n.sim) {
+    dd[i] <- dip(runif(N))
+    if(i %% 100 == 0) {
+        cat(".")
+        if(i %% 10000 == 0) {
+            cat("",i,"\n")
+            .opt <- .pt ; .pt <- proc.time(); .pt - .opt
+        }
+    }
+}
+
+nam <- paste("dip", floor(N/1000),"k", sep='')
+assign(nam, dd)
+outf <- paste(nam, "rda", sep=".")
+save(dd, file = outf)
