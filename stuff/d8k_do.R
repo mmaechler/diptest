a41c332293fcf453538c16217f6b91765f11a43f
Martin Maechler <maechler@r-project.org>
2008-12-19 17:29:53 +0000

    version of 2008.12.19.17.29.53 (now is 30 Nov 2020).

--- stuff/d8k_do.R
+++ stuff/d8k_do.R
@@ -1,19 +1,5 @@
-stopifnot(require("diptest"))
-setwd("/u/maechler/R/Pkgs/diptest/stuff")
-
-N <- 8000
+N <- 20000
 n.sim <- 1000001
-d8k <- numeric(n.sim)
-.pt <- proc.time()
+set.seed(12)
+source("/u/maechler/R/Pkgs/diptest/stuff/dip_N_do.R", echo=TRUE)
 
-set.seed(123)
-for(i in 1:n.sim) {
-    d8k[i] <- dip(runif(N))
-    if(i %% 100 == 0) {
-        cat(".")
-        if(i %% 10000 == 0) {
-            cat("",i,"\n")
-            .opt <- .pt ; .pt <- proc.time(); .pt - .opt
-        }
-    }
-}
