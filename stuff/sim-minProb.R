f006828743864667569a2756dd44478df4ff8db9
Martin Maechler <maechler@r-project.org>
2009-04-14 10:48:03 +0000

    version of 2009.04.14.10.48.03 (now is 30 Nov 2020).

--- stuff/sim-minProb.R
+++ stuff/sim-minProb.R
@@ -6,17 +6,16 @@
 ## and hence the distribution of D(X), X ~ U[0,1] starts with a jump,
 ## from 0 to pm(n) at  d = 1/(2n).
 
+### Now estimate pm(n) via simulation :
+
 ### This simulation is analyzed in file ./minProb-anal.R
 ##                                        ~~~~~~~~~~~~~~
-
-### Now estimate pm(n) via simulation :
 
 library(diptest)
 
 Ns <- 100000 # number of samples (per n)
 isim <- 1:Ns
 nn <- c(4:20,25,30,40,50)
-
 
 nMin <- sapply(nn, function(n)
                sum(sapply(isim,
