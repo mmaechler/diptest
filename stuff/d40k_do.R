04e1e603d5674984e57b11fb73a44858dd4cf259
Martin Maechler <maechler@r-project.org>
2009-04-15 11:51:17 +0000

    Initial revision

---
+++ stuff/d40k_do.R
@@ -0,0 +1,5 @@
+N <- 40000
+n.sim <- 1000001
+set.seed(67)
+source("/u/maechler/R/Pkgs/diptest/stuff/dip_N_do.R", echo=TRUE)
+
---
+++ stuff/d52k_do.R
@@ -0,0 +1,5 @@
+N <- 52000
+n.sim <- 1000001
+set.seed(109)
+source("/u/maechler/R/Pkgs/diptest/stuff/dip_N_do.R", echo=TRUE)
+
