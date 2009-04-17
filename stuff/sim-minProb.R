d8a669c474f3378cbd8eb9b3a58abf8bce422ff8
Martin Maechler <maechler@r-project.org>
2009-04-17 19:01:44 +0000

    version of 2009.04.17.19.01.44 (now is 30 Nov 2020).

--- stuff/sim-minProb.R
+++ stuff/sim-minProb.R
@@ -1,21 +1,21 @@
 
-### If  D = D(X_1, ... X_n) is the dip statistic, we have
-###       D >= 1/(2 n)
+### If  D_n = D(X_1, ... X_n) is the dip statistic, we have
+###     D_n >= 1/(2 n)
 ###   pm(n) := Pr[ D == 1/(2 n) ]  > 0
 ###   ==      ------------------------
-## and hence the distribution of D(X), X ~ U[0,1] starts with a jump,
+## and hence the distribution of D_n(X), X ~ U[0,1] starts with a jump,
 ## from 0 to pm(n) at  d = 1/(2n).
 
-### Now estimate pm(n) via simulation :
-
-### This simulation is analyzed in file ./minProb-anal.R
-##                                        ~~~~~~~~~~~~~~
+### Now estimate pm(n) via simulation.
+### The simulation is analyzed in file ./minProb-anal.R
+###                                      ~~~~~~~~~~~~~~
+setwd("~/R/Pkgs/diptest/stuff")
 
 library(diptest)
 
-Ns <- 100000 # number of samples (per n)
+Ns <- 500000 # number of samples (per n)
 isim <- 1:Ns
-nn <- c(4:20,25,30,40,50)
+nn <- c(4:18,20,25,30,35,40,50,60, 75, 100)
 
 nMin <- sapply(nn, function(n)
                sum(sapply(isim,
@@ -25,4 +25,5 @@ attr(nMin, "Ns") <- Ns
 ## nMin / Ns == pm(n), i.e. pm(nn)
 save(nMin, file= "minProb.rda")
 
-q('no')
+proc.time()
+sessionInfo()
